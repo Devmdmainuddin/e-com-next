b15f562437325ea9fd117793fb68f449b1199d87
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-12-06 11:47:42 +0600

    blog page edit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,6 +4,26 @@ const blog = () => {
     return (
         <div>
            <h2>blog page</h2> 
+           <div>
+             {/* Blog posts */}
+             <div>
+                 <h3>Blog post 1</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+             </div>
+             <div>
+                 <h3>Blog post 2</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+             </div>
+             <div>
+                 <h3>Blog post 3</h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+             </div>
+             {/* Pagination */}
+             <div>
+                 <a href="#" className="prev">Previous</a>
+                 <a href="#" className="next">Next</a>
+             </div>
+           </div>
         </div>
     );
 };
